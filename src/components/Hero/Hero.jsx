52e8f661cdedd42d4b96176a25496f6870d87a90
11saran <1211saran@gmail.com>
2025-01-25 16:14:40 +0530

    Refactor Hero component styles and structure; add new text and button elements

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,41 @@ const Hero = () => {
           <span>The best fitness clup in the town</span>
         </div>
         {/* Hero Heading*/}
-        
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+        {/* Figures*/}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+ 978</span>
+            <span>Members joined</span>
+          </div>
+          <div>
+            <span>+ 50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/*Hero Buttons*/}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
       </div>
       <div className="right-h">Right side</div>
     </div>
