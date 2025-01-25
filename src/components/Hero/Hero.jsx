814d9cdda3500972a1019f0b98baa2210a2e2d4a
11saran <1211saran@gmail.com>
2025-01-25 15:15:01 +0530

    Add Header and Hero components with styling and layout adjustments

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header';
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
 const Hero = () => {
   return (
     <div className="hero">
       <div className="left-h">
-        <Header/>
+        <Header />
+        {/*the best ad*/}
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness clup in the town</span>
+        </div>
+        {/* Hero Heading*/}
+        
       </div>
-      <div className="left-h">Right side</div>
+      <div className="right-h">Right side</div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
